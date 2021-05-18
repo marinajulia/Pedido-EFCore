807afd52eef2527b2cc9f15fbe9c67c86f58e1db
Marina Julia <marinajulia707@gmail.com>
2021-05-18 10:03:44 -0300

    Criando migração

--- CursoEntityFramework1/Program.cs
+++ CursoEntityFramework1/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace CursoEntityFramework1 {
     class Program {
         static void Main(string[] args) {
+
+            using var db = new Data.ApplicationContext();
+            db.Database.Migrate();
             Console.WriteLine("Hello World!");
         }
     }
