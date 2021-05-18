02eb01f088f54fd7db4586cf93b6f31fd2ded2e4
Marina Julia <marinajulia707@gmail.com>
2021-05-17 22:21:46 -0300

    Usando DataAnnotations

--- CursoEntityFramework1/Domain/Cliente.cs
+++ CursoEntityFramework1/Domain/Cliente.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoEntityFramework1.Domain {
+    [Table("Clientes")]
     public class Cliente {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Column("Phone")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
