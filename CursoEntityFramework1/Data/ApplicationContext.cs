43acb047d2950664f4010c6ec528614ec180be2a
Marina Julia <marinajulia707@gmail.com>
2021-05-24 17:02:39 -0300

    adicionando conexão resiliente

--- CursoEntityFramework1/Data/ApplicationContext.cs
+++ CursoEntityFramework1/Data/ApplicationContext.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoEntityFramework1.Data {
-   public class ApplicationContext:DbContext {
+    public class ApplicationContext : DbContext {
         private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
@@ -19,10 +19,25 @@ namespace CursoEntityFramework1.Data {
             optionsBuilder
                 .UseLoggerFactory(_logger)
                 .EnableSensitiveDataLogging()//mostra os dados
-                .UseSqlServer(@"Data Source=DESKTOP-8024PRG\SERVIDOR;Initial Catalog=CursoEFCore;Integrated Security=True");
+                .UseSqlServer(@"Data Source=DESKTOP-8024PRG\SERVIDOR;Initial Catalog=CursoEFCore;Integrated Security=True",
+                p=>p.EnableRetryOnFailure(maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null));
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder) {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes()) {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+                foreach (var property in properties) {
+                    if (string.IsNullOrEmpty(property.GetColumnType()) && !property.GetMaxLength().HasValue) {
+                        //property.SetMaxLength(100);
+                        property.SetColumnType("VARCHAR(100)"); //quando achar algum sem estar definido, vai ser esse
+                    }
+                }
+            }
         }
     }
 }
